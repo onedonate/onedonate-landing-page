ac0f79e2db4d551c4a8b66f1db7f1dd3a88160c6
lawnguyen <law.lnguyen@gmail.com>
2021-10-06 02:02:05 -0600

    extend custom colors in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,21 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "accent-purple": "#BFA2DB",
+        "brand-purple": "#F0D9FF",
+        "accent-gray": "#7F7C82",
+        "light-accent-gray": "#F2F2F4",
+      },
+    },
     container: {
       center: true,
       padding: "1rem",
       screens: {
-        lg: "1124px",
-        xl: "1124px",
-        "2xl": "1124px",
+        lg: "1224px",
+        xl: "1224px",
+        "2xl": "1224px",
       },
     },
     fontFamily: {
