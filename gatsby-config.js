ae25d5d079d37210d77f7ef47bf617fa67f1fd01
lawnguyen <law.lnguyen@gmail.com>
2021-10-05 23:31:42 -0600

    install and configure tailwind

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,7 @@ module.exports = {
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
+    "gatsby-plugin-postcss",
     {
       resolve: "gatsby-source-filesystem",
       options: {
