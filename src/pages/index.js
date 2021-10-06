497b39029a262678d36c8e81c5406caa744c0a4d
lawnguyen <law.lnguyen@gmail.com>
2021-10-06 02:08:04 -0600

    fix: use react-helmet for page metadata

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,13 @@
 import * as React from "react";
+import { Helmet } from "react-helmet";
 import LandingPage from "../components/LandingPage";
 
 const IndexPage = () => {
   return (
     <main class="font-Epilogue">
-      <title>Onedonate - A centralized platform for giving</title>
+      <Helmet>
+        <title>Onedonate - A centralized platform for giving</title>
+      </Helmet>
       <LandingPage></LandingPage>
     </main>
   );
