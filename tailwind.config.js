41fa1bc623591494755ddafa76dfde4af036e92a
lawnguyen <law.lnguyen@gmail.com>
2021-10-06 01:22:25 -0600

    configure tailwind for container theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,15 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    container: {
+      center: true,
+      padding: "1rem",
+      screens: {
+        lg: "1124px",
+        xl: "1124px",
+        "2xl": "1124px",
+      },
+    },
   },
   variants: {
     extend: {},
