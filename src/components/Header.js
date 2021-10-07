5e759c035af786877892da3f050020255e2ac511
lawnguyen <law.lnguyen@gmail.com>
2021-10-07 02:51:19 -0600

    fix: change class to className

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,9 +4,13 @@ import logo from "../images/onedonate.svg";
 const Header = () => {
   return (
     <header>
-      <nav class="container flex items-center pt-4 pb-8 mt-4 sm:mt-12">
-        <div class="py-1">
-          <img class="h-8 w-auto sm:h-10" src={logo} alt="Onedonate logo"></img>
+      <nav className="container flex items-center pt-4 pb-8 mt-4 sm:mt-12">
+        <div className="py-1">
+          <img
+            className="h-8 w-auto sm:h-10"
+            src={logo}
+            alt="Onedonate logo"
+          ></img>
         </div>
       </nav>
     </header>
--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -3,40 +3,40 @@ import graphic from "../images/landing-graphic.svg";
 
 const LandingPage = () => {
   return (
-    <section class="relative">
-      <div class="container flex flex-col-reverse lg:flex-row items-center">
-        <div class="flex flex-1 flex-col items-center lg:items-start">
-          <h2 class="font-bold text-brand-purple text-5xl md:text-4 lg:text-7xl text-center lg:text-left mb-6">
+    <section className="relative">
+      <div className="container flex flex-col-reverse lg:flex-row items-center">
+        <div className="flex flex-1 flex-col items-center lg:items-start">
+          <h2 className="font-bold text-brand-purple text-5xl md:text-4 lg:text-7xl text-center lg:text-left mb-6">
             Donating made simple
           </h2>
-          <p class="text-accent-gray text-lg text-center lg:text-left mb-6">
+          <p className="text-accent-gray text-lg text-center lg:text-left mb-6">
             A centralized donation platform that empowers you to support{" "}
-            <span class="underline italic">all</span> the causes that you care
-            about. We aim to support over 86000 charities so you can research
-            organizations, build your donation pie, and contribute to a better
-            world all in one place.
+            <span className="underline italic">all</span> the causes that you
+            care about. We aim to support over 86000 charities so you can
+            research organizations, build your donation pie, and contribute to a
+            better world all in one place.
           </p>
-          <p class="text-accent-gray text-lg text-center lg:text-left mb-6">
+          <p className="text-accent-gray text-lg text-center lg:text-left mb-6">
             Sign up for updates today to get early access!
           </p>
-          <div class="w-full flex flex-row gap-4 flex-wrap lg:flex-nowrap">
+          <div className="w-full flex flex-row gap-4 flex-wrap lg:flex-nowrap">
             <input
-              class="px-4 py-3 focus:border-transparent focus:ring-2 focus:ring-brand-purple rounded-md border-gray-400 shadow-md w-full"
+              className="px-4 py-3 focus:border-transparent focus:ring-2 focus:ring-brand-purple rounded-md border-gray-400 shadow-md w-full"
               type="text"
               placeholder="Enter your email address"
             ></input>
             <button
               type="button"
-              class="hover:bg-gray-700 shadow-md py-3 px-6 rounded-md transition duration-150 bg-gray-600 text-white"
+              className="hover:bg-gray-700 shadow-md py-3 px-6 rounded-md transition duration-150 bg-gray-600 text-white"
             >
               Sign up
             </button>
           </div>
         </div>
 
-        <div class="flex justify-center flex-1 mb-10 md:mb-16 lg:mb-0">
+        <div className="flex justify-center flex-1 mb-10 md:mb-16 lg:mb-0">
           <img
-            class="w-5/6 h-5/6 sm:w-3/4 sm:h-3/4 md:w-full md:h-full"
+            className="w-5/6 h-5/6 sm:w-3/4 sm:h-3/4 md:w-full md:h-full"
             src={graphic}
             alt="two excited characters using their electronic devices"
           ></img>
