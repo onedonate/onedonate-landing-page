f885f81a050123e4ba5790cd6c51b397b24db363
lawnguyen <law.lnguyen@gmail.com>
2021-10-08 00:53:00 -0600

    refactor: extract grpahic component

---
+++ src/components/Graphic.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Graphic = ({ graphic }) => {
+  return (
+    <div className="flex justify-center flex-1 mb-10 md:mb-16 lg:mb-0">
+      <img
+        className="w-5/6 h-5/6 sm:w-3/4 sm:h-3/4 md:w-full md:h-full"
+        src={graphic}
+        alt="two excited characters using their electronic devices"
+      ></img>
+    </div>
+  );
+};
+
+export default Graphic;
--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Graphic from "./Graphic";
 import graphic from "../images/landing-graphic.svg";
 
 const LandingPage = () => {
@@ -34,13 +35,7 @@ const LandingPage = () => {
           </div>
         </div>
 
-        <div className="flex justify-center flex-1 mb-10 md:mb-16 lg:mb-0">
-          <img
-            className="w-5/6 h-5/6 sm:w-3/4 sm:h-3/4 md:w-full md:h-full"
-            src={graphic}
-            alt="two excited characters using their electronic devices"
-          ></img>
-        </div>
+        <Graphic graphic={graphic}></Graphic>
       </div>
     </section>
   );
