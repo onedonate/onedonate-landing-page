38d1aac352bd72370a4b02a00abbaa9a20043465
lawnguyen <law.lnguyen@gmail.com>
2021-10-08 01:53:10 -0600

    revert: temporary message for live deploy

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,18 @@
 import * as React from "react";
-// import { Helmet } from "react-helmet";
-// import Header from "../components/Header";
-// import LandingPage from "../components/LandingPage";
-// import Features from "../components/Features";
+import { Helmet } from "react-helmet";
+import Header from "../components/Header";
+import LandingPage from "../components/LandingPage";
+import Features from "../components/Features";
 
 const IndexPage = () => {
   return (
     <main className="font-Epilogue">
-      <h2 className="font-bold text-brand-purple text-5xl md:text-4 lg:text-7xl text-center lg:text-left mb-6">
-        Coming soon...
-      </h2>
+      <Helmet>
+        <title>Onedonate - A centralized platform for giving</title>
+      </Helmet>
+      <Header></Header>
+      <LandingPage></LandingPage>
+      <Features></Features>
     </main>
   );
 };
