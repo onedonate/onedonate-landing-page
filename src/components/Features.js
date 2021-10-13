bfaad2613a4bfa41319243132874942dcf34efa6
lawnguyen <law.lnguyen@gmail.com>
2021-10-13 17:55:23 -0600

    feature: implement feature component

---
+++ src/components/Features.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Features = () => {
+  return (
+    <section className="bg-light-accent-gray py-20 mt-20">
+      <div className="container grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-16 max-w-screen-lg">
+        <div className="flex flex-col rounded-md shadow-md">
+          <div className="flex flex-col items-center">Hello</div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Features;
