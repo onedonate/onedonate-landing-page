4ff24e543c1f13123c8e1aebfefecebbf8139397
Jeffbanawa <jeffrey.banawa@gmail.com>
2021-10-13 22:37:26 -0600

    style: text for Features section

--- src/components/Features.js
+++ src/components/Features.js
@@ -7,7 +7,16 @@ import icon3 from "../images/features-icon3.svg";
 const Features = () => {
   return (
     <section className="bg-light-accent-gray py-20 mt-20">
-      <div className="container grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-16 max-w-screen-lg">
+      <div className="mx-auto max-w-md px-4 text-center sm:max-w-3xl sm:px-6 lg:px-8 lg:max-w-7xl">
+        <h2 className="text-base font-semibold tracking-wider text-brand-blue uppercase">
+          FEATURES
+        </h2>
+        <p className="mt-2 text-3xl font-extrabold tracking-tight sm:text-4xl">
+          Everything you need to make an impact
+        </p>
+      </div>
+
+      <div className="container grid grid-cols-1 mt-12 md:grid-cols-2 lg:grid-cols-3 gap-16 max-w-screen-lg">
         <Card
           icon={icon1}
           title="Improve your donation experience"
