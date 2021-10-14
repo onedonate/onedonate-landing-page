0feeb81cf27b60ce2191b491ace86c1f4c3be505
Jeffbanawa <jeffrey.banawa@gmail.com>
2021-10-13 21:22:44 -0600

    feature: implement icon and description for Card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,18 @@
 import React from "react";
 
-const Card = ({ title }) => {
+const Card = ({ icon, title, description }) => {
   return (
-    <div className="flex flex-col rounded-md shadow-md">
-      <div className="flex flex-col items-center">{title}</div>
+    <div className="bg-white flex flex-col rounded-md shadow-md">
+      <div className="flex flex-col items-left m-8">
+        <div>
+          <img
+            src={icon}
+            alt="two excited characters using their electronic devices"
+          ></img>
+        </div>
+        <div className="font-bold mb-2">{title}</div>
+        <div className="text-sm">{description}</div>
+      </div>
     </div>
   );
 };
--- src/components/Features.js
+++ src/components/Features.js
@@ -1,13 +1,28 @@
 import React from "react";
 import Card from "./Card";
+import icon1 from "../images/features-icon1.svg";
+import icon2 from "../images/features-icon2.svg";
+import icon3 from "../images/features-icon3.svg";
 
 const Features = () => {
   return (
     <section className="bg-light-accent-gray py-20 mt-20">
       <div className="container grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-16 max-w-screen-lg">
-        <Card title="Hello"></Card>
-        <Card title="HI"></Card>
-        <Card title="Hey"></Card>
+        <Card
+          icon={icon1}
+          title="Improve your donation experience"
+          description="A simple donation process that allows you to browse thousands of charities and be able to donate within seconds."
+        ></Card>
+        <Card
+          icon={icon2}
+          title="Donation Options"
+          description="Provide options to donate once, recurring, and even to multiple charaties through a single donation."
+        ></Card>
+        <Card
+          icon={icon3}
+          title="Stress-free Tax Return"
+          description="Never lose another charitable tax receipt again. Choose to view individual tax receipts or a summary tax receipt at the end of each year to easily recieve a donation tax credit."
+        ></Card>
       </div>
     </section>
   );
