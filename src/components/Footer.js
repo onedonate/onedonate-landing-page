7b1b1c277b3c5dd649d565dd9bcdc634189b6d0e
Jeffbanawa <jeffrey.banawa@gmail.com>
2021-10-14 00:52:10 -0600

    style: rounded borders

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Footer = () => {
-  return <section className="bg-back-blue py-20 mt-20"></section>;
+  return <section className="bg-back-blue py-20 mt-20 rounded-t-3xl"></section>;
 };
 
 export default Footer;
