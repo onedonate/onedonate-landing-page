de153d889c97eda31c057b5b75cfc5ee7d7bff0c
lawnguyen <law.lnguyen@gmail.com>
2021-11-25 01:56:30 -0800

    feat: add onClick prop to Button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,10 @@
 import React from "react";
 
-const Button = ({ text }) => {
+const Button = ({ text, onClick }) => {
   return (
     <button
       type="button"
+      onClick={onClick}
       className="hover:bg-gray-700 shadow-md py-3 px-6 rounded-md transition duration-150 bg-gray-600 text-white w-full lg:w-32"
     >
       {text}
