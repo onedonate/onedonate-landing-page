5cb78a8cbce1dc6c583b55e210e60d7c97a50961
lawnguyen <law.lnguyen@gmail.com>
2021-11-25 02:07:56 -0800

    style: update brand name in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,10 +5,10 @@ const Footer = () => {
     <section className="bg-back-blue py-10 mt-20 rounded-t-3xl">
       <div className="container grid grid-cols-1 text-white md:grid-cols-2 lg:grid-cols-3 gap-x-40 max-w-7xl">
         <div className="flex flex-col items-left text-left text-large p-4">
-          <div className="font-bold text-lg mb-2">OneDonate</div>
+          <div className="font-bold text-lg mb-2">Onedonate</div>
           <ul>A: Canada</ul>
           <ul>T: 403-671-7564</ul>
-          <ul>E: Onedonate@outlook.com</ul>
+          <ul>E: onedonate@outlook.com</ul>
         </div>
         <div className="flex flex-col items-left text-left text-large p-4">
           <div className="font-bold text-lg mb-2">About</div>
