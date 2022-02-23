6c12e7a38ba74afc97a8a41e90634b68cdec8ebb
Jeffbanawa <jeffrey.banawa@gmail.com>
2022-02-22 19:33:08 -0700

    feat: fade in hero graphic

--- src/components/Graphic.js
+++ src/components/Graphic.js
@@ -1,14 +1,25 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 const Graphic = ({ graphic, width, alt }) => {
   return (
-    <div className="flex justify-center flex-1 my-10 md:my-16 lg:my-0">
+    <motion.div
+      initial="hidden"
+      whileInView="visible"
+      viewport={{ once: true }}
+      transition={{ duration: 0.8 }}
+      variants={{
+        visible: { opacity: 1, scale: 1 },
+        hidden: { opacity: 0, scale: 0.9 },
+      }}
+      className="flex justify-center flex-1 my-10 md:my-16 lg:my-0"
+    >
       <img
         className={`w-5/6 h-5/6 sm:w-3/4 sm:h-3/4 md:w-${width} md:h-full `}
         src={graphic}
         alt={alt}
       ></img>
-    </div>
+    </motion.div>
   );
 };
 
