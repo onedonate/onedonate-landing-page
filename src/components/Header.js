4115a7d4db05990a310fb558802e97e3a83088c5
Jeffbanawa <jeffrey.banawa@gmail.com>
2022-02-23 22:48:07 -0700

    feat: animate header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,28 @@
 import React from "react";
 import logo from "../images/Logo.svg";
+import { motion } from "framer-motion";
 
 const Header = () => {
   return (
     <header>
       <nav className="container flex items-center pt-1 pb-8 mt-1 max-w-7xl sm:mt-4">
-        <div className="py-1">
+        <motion.div
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true }}
+          transition={{ duration: 1.0 }}
+          variants={{
+            visible: { y: 0, opacity: 1, scale: 1 },
+            hidden: { y: -20, opacity: 0, scale: 1 },
+          }}
+          className="py-1"
+        >
           <img
             className="h-6 w-auto md:h-8"
             src={logo}
             alt="Onedonate logo"
           ></img>
-        </div>
+        </motion.div>
       </nav>
     </header>
   );
