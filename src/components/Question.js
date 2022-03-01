021837a6d08058f7c940b42ff63b96ac1a087f25
Jeffbanawa <jeffrey.banawa@gmail.com>
2022-02-28 19:16:11 -0700

    feat: change to flexbox and animate questions

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,46 +1,54 @@
 import React from "react";
 import Description from "./Description";
-import {motion, AnimatePresence } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 
 const Question = ({ question, answer }) => {
-    const [showAnswers, setShowAnswers] = React.useState(false);
-    const onClick = () => setShowAnswers(!showAnswers);
+  const [showAnswers, setShowAnswers] = React.useState(false);
+  const onClick = () => setShowAnswers(!showAnswers);
 
   return (
     <div className="border-b">
-        <div className="flex justify-between gap-x-8 mx-auto cursor-pointer" onClick={onClick}>
-            <h2 className="font-Montserrat font-semibold text-dark-gray text-lg text-left leading-normal my-auto lg:text-2xl">{question}</h2>
-            <div className="p-2 my-auto">
-                <motion.h2 
-                    initial={{ rotate: 0 }}
-                    animate={{
-                    rotate: showAnswers ? -45 : 0,
-                    }}
-                    className="font-Montserrat text-brand-green text-4xl text-left leading-normal mb-2 p-2 lg:mb-6"
-                >+
-                </motion.h2>
-            </div>
-            
+      <div
+        className="flex justify-between gap-x-8 mx-auto cursor-pointer"
+        onClick={onClick}
+      >
+        <h2 className="font-Montserrat font-semibold text-dark-gray text-lg text-left leading-normal my-auto py-4 lg:text-xl">
+          {question}
+        </h2>
+        <div className="p-2 my-auto">
+          <motion.h2
+            initial={{ rotate: 0 }}
+            animate={{
+              rotate: showAnswers ? 180 : 0,
+              opacity: [1, 0.5, 1],
+            }}
+            className="font-Montserrat text-brand-green text-4xl text-left leading-normal "
+          >
+            {showAnswers ? "-" : "+"}
+          </motion.h2>
         </div>
-        
-        <AnimatePresence initial={false}>
-            { showAnswers ? 
-                <motion.div 
-                    key="content"
-                    initial="collapsed"
-                    animate="open"
-                    exit="collapsed"
-                    variants={{
-                        open: { x: 0, opacity: 1, height: "auto" },
-                        collapsed: { x:-50, opacity: 0, height: 0 }
-                    }}
-                    transition={{ duration: 0.3, linear: 1}}
-                >
-                    <Description text={answer} />
-                </motion.div> : null }
-        </AnimatePresence>
-        
-        
+      </div>
+
+      <AnimatePresence initial={false}>
+        {showAnswers ? (
+          <motion.div
+            key="content"
+            initial="collapsed"
+            animate="open"
+            exit="collapsed"
+            variants={{
+              open: { opacity: 1, height: "auto" },
+              collapsed: { opacity: 0, height: 0 },
+            }}
+            transition={{ duration: 0.3 }}
+            className="mt-4"
+          >
+            {answer.split("~").map((str) => (
+              <Description text={str} />
+            ))}
+          </motion.div>
+        ) : null}
+      </AnimatePresence>
     </div>
   );
 };
