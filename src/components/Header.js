10cc6c76708d88b1e9b985b73ea392b97a9b8714
Jeffbanawa <jeffrey.banawa@gmail.com>
2022-04-28 16:20:55 -0600

    refactor: remove unused imports and linting

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,5 @@
 import React, { useState, useEffect } from "react";
 import DesktopNav from "./DesktopNav";
-import logo from "../images/Logo.svg";
-import { motion } from "framer-motion";
-import { Link, animateScroll as scroll } from "react-scroll";
 
 const Header = () => {
   const [scrollPosition, setScrollPosition] = useState(0);
@@ -12,15 +9,19 @@ const Header = () => {
   };
 
   useEffect(() => {
-    window.addEventListener('scroll', handleScroll, { passive: true });
+    window.addEventListener("scroll", handleScroll, { passive: true });
 
     return () => {
-      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener("scroll", handleScroll);
     };
   }, []);
 
   return (
-    <header className={`fixed bg-white z-50 h-12 w-full md:h-20 ${scrollPosition == 0 ? '' : 'md:shadow-md'}`}>
+    <header
+      className={`fixed bg-white z-50 h-12 w-full md:h-20 ${
+        scrollPosition === 0 ? "" : "md:shadow-md"
+      }`}
+    >
       <DesktopNav />
     </header>
   );
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 import * as React from "react";
-import { Helmet } from "react-helmet";
 import Header from "../components/Header";
 import LandingPage from "../components/LandingPage";
 import Footer from "../components/Footer";
