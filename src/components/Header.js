8c4481751cb76dc82d542eba89d00b3db651edd5
jeffbanawa <jeffrey.banawa@gmail.com>
2022-04-28 09:47:10 -0600

    style: shadow only when at top of page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import logo from "../images/Logo.svg";
 import { motion } from "framer-motion";
 import { Link, animateScroll as scroll } from "react-scroll";
 
 const Header = () => {
+  const [scrollPosition, setScrollPosition] = useState(0);
+  const handleScroll = () => {
+    const position = window.pageYOffset;
+    setScrollPosition(position);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll, { passive: true });
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <header className="fixed bg-white z-50 h-12 w-full md:h-20 md:shadow-md">
-      <nav className="container flex justify-between items-center pt-1 pb-8 mt-4 md:px-20">
+    <header className={`fixed bg-white z-50 h-12 w-full md:h-20 ${scrollPosition == 0 ? '' : 'md:shadow-md'}`}>
+      <nav className="container flex justify-between items-center pt-1 pb-8 mt-4 px-4 lg:px-20">
         <motion.div
           initial="hidden"
           whileInView="visible"
