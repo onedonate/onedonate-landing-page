f67d81ec1c5ff261d75983d9e5879c02022ca118
jeffbanawa <jeffrey.banawa@gmail.com>
2022-04-28 10:36:32 -0600

    refactor: extract DesktopNav component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import DesktopNav from "./DesktopNav";
 import logo from "../images/Logo.svg";
 import { motion } from "framer-motion";
 import { Link, animateScroll as scroll } from "react-scroll";
@@ -20,52 +21,7 @@ const Header = () => {
 
   return (
     <header className={`fixed bg-white z-50 h-12 w-full md:h-20 ${scrollPosition == 0 ? '' : 'md:shadow-md'}`}>
-      <nav className="container flex justify-between items-center pt-1 pb-8 mt-4 px-4 lg:px-20">
-        <motion.div
-          initial="hidden"
-          whileInView="visible"
-          viewport={{ once: true }}
-          transition={{ duration: 1.0 }}
-          variants={{
-            visible: { y: 0, opacity: 1, scale: 1 },
-            hidden: { y: -20, opacity: 0, scale: 1 },
-          }}
-          className="py-1"
-        >
-          <img
-            className="h-5 w-auto cursor-pointer md:h-6"
-            src={logo}
-            alt="Onedonate logo"
-            onClick={scroll.scrollToTop}
-          ></img>
-        </motion.div>
-        <ul className="flex items-center gap-4 md:gap-14">
-          <Link
-            activeClass="active"
-            to="features"
-            spy={true}
-            smooth={true}
-            offset={0}
-            duration={1000}
-          >
-            <li type="button" className="cursor-pointer">
-              Features
-            </li>
-          </Link>
-          <Link
-            activeClass="active"
-            to="faq"
-            spy={true}
-            smooth={true}
-            offset={0}
-            duration={1000}
-          >
-            <li type="button" className="cursor-pointer">
-              FAQ
-            </li>
-          </Link>
-        </ul>
-      </nav>
+      <DesktopNav />
     </header>
   );
 };
